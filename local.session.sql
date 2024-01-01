66293289f89c0276bab4c3b07a306d2538364b3e
Solida <solida.var@gmail.com>
2024-01-01 03:03:30 +0100

    cambios

--- local.session.sql
+++ local.session.sql
@@ -11,6 +11,5 @@
 SELECT departamento.nombre AS nombre_departamento, COUNT(profesor.id_profesor) AS numero_profesores
 FROM universidad.departamento
 JOIN universidad.profesor ON departamento.id = profesor.id_departamento
-GROUP BY departamento.nombre
-HAVING COUNT(profesor.id_profesor) > 0
+WHERE COUNT(profesor.id_profesor) > 0
 ORDER BY COUNT(profesor.id_profesor) DESC;
