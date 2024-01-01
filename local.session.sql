270c44b4c350e838dea7b5c5c8ff7ce1b7bd9c4e
Solida <solida.var@gmail.com>
2024-01-01 03:04:47 +0100

    Fix bug in login functionality

--- local.session.sql
+++ local.session.sql
@@ -11,5 +11,6 @@
 SELECT departamento.nombre AS nombre_departamento, COUNT(profesor.id_profesor) AS numero_profesores
 FROM universidad.departamento
 JOIN universidad.profesor ON departamento.id = profesor.id_departamento
-WHERE COUNT(profesor.id_profesor) > 0
+GROUP BY departamento.nombre
+HAVING COUNT(profesor.id_profesor) > 0
 ORDER BY COUNT(profesor.id_profesor) DESC;
